bc0fb1c01566e1b1cd933464a5b7cb56dd3a6c28
Michael Lennartz <michael@dubspeed.de>
2017-03-22 15:55:57 +0100

    updated with tank game page

--- shooty/test/enemy-test.js
+++
@@ -1,9 +0,0 @@
-buster.testCase( "Enemy", {
-    "basic tests": {
-        "exists etc.": function() {
-            assert.isObject( Enemy );
-            assert.equals( Object.getPrototypeOf( Enemy ), "Sprite" );
-            assert.equals( Enemy.toString(), "Enemy" );
-        }
-    }
-});
